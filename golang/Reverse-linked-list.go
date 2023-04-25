469561f2484b831fe3484078c7e4ac8ea536dfbb
Duong Nguyen <me@duong-nguyen.com>
2023-04-25 13:06:14 +0300

    Move all javas solution into java folder

---
+++ golang/Reverse-linked-list.go
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+ import "fmt"
+
+ func reverseList(head *ListNode) *ListNode {
+		arr := []int{}
+    if head == nil {
+			return nil
+		} else {
+			for head != nil {
+				arr = append(arr, head.Val)
+				head = head.Next
+			}
+	}
+	
+	//reverse the array
+	for i, j := 0, len(arr)-1; i < j; i, j = i+1, j-1 {
+        arr[i], arr[j] = arr[j], arr[i]
+  }
+
+	//create empty listnode - tmp and dummy point to the empty node
+	tmp := &ListNode{}
+	dummy := tmp
+
+	for node := range arr {
+		fmt.Printf("%+v ->", node)
+	}
+
+	for _, node := range arr {
+		//fmt.Printf("%+v ->", node)
+		tmp.Next = &ListNode{node, nil}
+		tmp = tmp.Next
+	}
+	tmp.Next = nil
+	return dummy.Next
+}
