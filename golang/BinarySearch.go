20c45afb7df2a2cfae501e7dcb8de133dfc4d7c5
Duong Nguyen <tynshjt@gmail.com>
2023-10-29 21:10:50 +0200

    BinarySearch increase runtime

--- golang/BinarySearch.go
+++ golang/BinarySearch.go
@@ -1,15 +1,16 @@
-//  Runtime: Beats 61.06%of users with Go
+//  Runtime: Beats 91.06%of users with Go
 //  Memory: Beats 100% users with Go
   
 func search(nums []int, target int) int {
-    return searchRecur(nums, target, 0, len(nums)-1);
+    end := len(nums)-1
+    return searchRecur(nums, target, 0, end)
 }
 
-func searchRecur(nums []int, target int, left int, right int) int {
-    var mid int = left + (right-left)/2;
+func searchRecur(nums []int, target , left , right int) int {
     if (left>right){
         return -1
     }
+    mid  := left + (right-left)/2;
     if nums[mid]==target {
         return mid
     } else if nums[mid] < target {
@@ -18,3 +19,23 @@ func searchRecur(nums []int, target int, left int, right int) int {
         return searchRecur(nums,target,left, mid-1)
     }
 }
+
+
+
+/*
+func solB(nums []int, target int) int {
+    var res int = -1
+    left , right := 0, len(nums)-1
+    for (left <= right) {
+        var mid int = left + (right-left)/2;
+        if (nums[mid] > target){
+            right = mid -1
+        } else if (nums[mid]<target){
+            left = mid +1
+        } else{
+            return mid
+        }
+    }
+    return res
+}
+/*
